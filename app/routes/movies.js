cd80fdd510e446ab8461c028f31d477edd01fbc7
Jonas <ruberg_bendtsen@hotmail.com>
2017-01-03 05:21:05 +0100

    do

--- app/routes/movies.js
+++ app/routes/movies.js
@@ -1,35 +1,48 @@
-var express = require('express');
-var app = express();
+var express = require('express'); //call express library - gives access to bunch of http verbs
+var router = express.Router(); //get an instance of the express router
 var MongoClient = require('mongodb').MongoClient;
 var ObjectId = require('mongodb').ObjectID;
 var url = ('mongodb://localhost:27017/store');
 
+//server taking care of handling request (req) for data and then returning some information (res)
+//passed along variable req - has a params object - any variables that we pass along through the url
+//creating a get method
+//routing allows to give users access to different types of data, and can pass along information to the routes through the params variable of the request object
 
-// Create Routes for Prodocts
+// -------------------- GET (READ) - GET ALL MOVIES  -------------------- //
 
-//GET ALL MOVIES
-app.get('/movies', function(req, res) {
+//response to a get request to the /movies route
+router.get('/movies', function(req, res) {
 
+    //connecting to the mongodb
     MongoClient.connect(url, function(err, db) {
 
-        var collection = db.collection('movies');
+        //using the movie collection and store it a variable called collection
+        var movieCollection = db.collection('movies');
 
-        collection.find({}).toArray(function(err, data) {
+        //find - returns an array of documents from the moviecollection
+        movieCollection.find().toArray(function(err, data) {
 
+            //sending back the information as json data
             res.json(data);
             db.close();
         });
     });
 });
 
-//GET A MOVIE WITH A SPECIFIC ID
-app.get('/movies/:id', function(req, res) {
+// -------------------- GET (READ) - GET A MOVIE WITH A SPECIFIC ID  -------------------- //
 
+//response to a get request to the /movies/:id route
+router.get('/movies/:id', function(req, res) {
+
+    //connecting to the mongodb
     MongoClient.connect(url, function(err, db) {
 
-        var collection = db.collection('movies');
+        //using the movie collection
+        var movieCollection = db.collection('movies');
 
-        collection.findOne({'_id' : ObjectId(req.params.id)}, function(err, data) {
+        //findOne - returns a single document from the moviecollection
+        movieCollection.findOne({'_id' : ObjectId(req.params.id)}, function(err, data) {
 
             res.send(data);
             db.close();
@@ -37,14 +50,19 @@ app.get('/movies/:id', function(req, res) {
     });
 });
 
-//CREATE A MOVIE
-app.post('/movies', function(req, res) {
+// -------------------- POST (CREATE) - CREATE A MOVIE -------------------- //
 
+//response to a post request to the /movies route
+router.post('/movies', function(req, res) {
+
+    //connecting to the mongodb
     MongoClient.connect(url, function(err, db) {
 
-        var collection = db.collection('movies');
+        //using the movie collection
+        var movieCollection = db.collection('movies');
 
-        collection.insert(req.body, function(err, data) {
+        //insert - creates a new document in the moviecollection
+        movieCollection.insert(req.body, function(err, data) { //req.body - contains key-value pairs of data submitted in the request body.
 
             res.send({"msg" : "Movie created"});
             db.close();
@@ -52,31 +70,43 @@ app.post('/movies', function(req, res) {
     });
 });
 
-// UPDATE MOVIE WITH SPECIFIC ID
-app.put('/movies/:id', function(req, res) {
 
+// -------------------- PUT (UPDATE) - UPDATE A MOVIE WITH A SPECIFIC ID  -------------------- //
+
+//response to a put request to the /movies/:id route
+router.put('/movies/:id', function(req, res) {
+
+    //connecting to the mongodb
     MongoClient.connect(url, function(err, db) {
 
-        var collection = db.collection('movies');
+        //using the movie collection
+        var movieCollection = db.collection('movies');
 
         delete req.body._id;
 
-        collection.update({'_id' : ObjectId(req.params.id)}, req.body, function(err, data) {
+        //update - modifies an existing document in the moviecollection
+        movieCollection.update({'_id' : ObjectId(req.params.id)}, req.body, function(err, data) {
 
+            //send a message that the movie is updated
             res.send({"msg" : "Movie updated"});
             db.close();
         });
     });
 });
 
-// DELETE MOVIE WITH A SPECIFIC ID
-app.delete('/movies/:id', function(req, res) {
+// -------------------- DELETE (DELETE) - DELETE A MOVIE WITH A SPECIFIC ID  -------------------- //
 
+//response to a delete request to the /movies/:id route
+router.delete('/movies/:id', function(req, res) {
+
+    //connecting to the mongodb and executes a callback function
     MongoClient.connect(url, function(err, db) {
 
-        var collection = db.collection('movies');
+        //using the movie collection
+        var movieCollection = db.collection('movies');
 
-        collection.remove({'_id' : ObjectId(req.params.id)}, function(err, data) {
+        //remove a single document from the moviecollection
+        movieCollection.remove({'_id' : ObjectId(req.params.id)}, function(err, data) {
 
             res.send({"msg" : "Movie deleted"});
             db.close();
@@ -84,5 +114,6 @@ app.delete('/movies/:id', function(req, res) {
     });
 });
 
-module.exports = app;
+// -------------------- ROUTING -------------------- //
+module.exports = router; //exports the contents of a module
 
--- app/server.js
+++ app/server.js
@@ -1,16 +1,22 @@
-var express = require('express');
-var app = express(); //create the app
+//call the packages we need
+var express = require('express'); //call express
+var app = express(); //define our app using express
 var cors = require('cors');
+var BodyParser = require('body-parser'); // middleware
 
-var BodyParser = require('body-parser'); // middle
-app.use(BodyParser.urlencoded({extended: true}));
-app.use(BodyParser.json());
+app.use(BodyParser.urlencoded({extended: true})); //for parsing application/json
+app.use(BodyParser.json()); //for parsing application/x-www-form-urlencoded
 
 app.use(cors());
 
-//Route - Movies
-var movies = require('./routes/movies.js');
-app.use(movies);
+//importing the movie route
+app.use(require('./routes/movies.js'));
+
+//404 - not found error message
+app.use(function(req, res){
+    res.status(404);
+    res.send({"message": "The server does not know the request you tried to make(404)"})
+});
 
 
 //Listening on port 3000
